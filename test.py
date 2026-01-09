b888c33f9390cbe14067d22d77fa4eb210674ad7
Croco Sian <sianh@qq.com>
2026-01-09 18:08:33 +0800

    234

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
+import math
+import os
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
+from torchdyn.datasets import generate_moons
+
+from optimal_transport import OTPlanSampler
+
+savedir = "models/8gaussian-moons"
+os.makedirs(savedir, exist_ok=True)
 
 
 def sample_conditional_pt(x0, x1, t, sigma):
@@ -49,12 +61,96 @@ def compute_conditional_vector_field(x0, x1):
     return x1 - x0
 
 
-batch_size = 32
-x0 = torch.randn((batch_size, 3, 32, 32))
-x1 = torch.randn((batch_size, 3, 32, 32))
-t = torch.rand((batch_size))
+class torch_wrapper(torch.nn.Module):
+    """Wraps model to torchdyn compatible format."""
 
-print(f"x0.shape: {x0.shape}")
-print(f"x1.shape: {x1.shape}")
-print(f"t.shape: {t.shape}")
-sample_conditional_pt(x0, x1, t, 1)
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, t, x, *args, **kwargs):
+        return self.model(torch.cat([x, t.repeat(x.shape[0])[:, None]], 1))
+
+
+def plot_trajectories(traj):
+    """Plot trajectories of some selected samples."""
+    n = 2000
+    plt.figure(figsize=(6, 6))
+    plt.scatter(traj[0, :n, 0], traj[0, :n, 1], s=10, alpha=0.8, c="black")
+    plt.scatter(traj[:, :n, 0], traj[:, :n, 1], s=0.2, alpha=0.2, c="olive")
+    plt.scatter(traj[-1, :n, 0], traj[-1, :n, 1], s=4, alpha=1, c="blue")
+    plt.legend(["Prior sample z(S)", "Flow", "z(0)"])
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+
+class MLP(torch.nn.Module):
+    def __init__(self, dim, out_dim=None, w=64, time_varying=False):
+        super().__init__()
+        self.time_varying = time_varying
+        if out_dim is None:
+            out_dim = dim
+        self.net = torch.nn.Sequential(
+            torch.nn.Linear(dim + (1 if time_varying else 0), w),
+            torch.nn.SELU(),
+            torch.nn.Linear(w, w),
+            torch.nn.SELU(),
+            torch.nn.Linear(w, w),
+            torch.nn.SELU(),
+            torch.nn.Linear(w, out_dim),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+def eight_normal_sample(n, dim, scale=1, var=1):
+    m = torch.distributions.multivariate_normal.MultivariateNormal(
+        torch.zeros(dim), math.sqrt(var) * torch.eye(dim)
+    )
+    centers = [
+        (1, 0),
+        (-1, 0),
+        (0, 1),
+        (0, -1),
+        (1.0 / np.sqrt(2), 1.0 / np.sqrt(2)),
+        (1.0 / np.sqrt(2), -1.0 / np.sqrt(2)),
+        (-1.0 / np.sqrt(2), 1.0 / np.sqrt(2)),
+        (-1.0 / np.sqrt(2), -1.0 / np.sqrt(2)),
+    ]
+    centers = torch.tensor(centers) * scale
+    noise = m.sample((n,))
+    multi = torch.multinomial(torch.ones(8), n, replacement=True)
+    data = []
+    for i in range(n):
+        data.append(centers[multi[i]] + noise[i])
+    data = torch.stack(data)
+    return data
+
+
+def sample_moons(n):
+    x0, _ = generate_moons(n, noise=0.2)
+    return x0 * 3 - 1
+
+
+def sample_8gaussians(n):
+    return eight_normal_sample(n, 2, scale=5, var=0.1).float()
+
+
+ot_sampler = OTPlanSampler(method="exact")
+sigma = 0.1
+dim = 2
+batch_size = 256
+model = MLP(dim=dim, time_varying=True)
+optimizer = torch.optim.Adam(model.parameters())
+
+start = time.time()
+for k in range(20000):
+    optimizer.zero_grad()
+
+    x0 = sample_8gaussians(batch_size)
+    x1 = sample_moons(batch_size)
+
+    # Draw samples from OT plan
+    x0, x1 = ot_sampler.sample_plan(x0, x1)
